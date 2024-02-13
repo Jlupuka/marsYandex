3e509046e4a1d6781d05ffadbd093fe0b31359cd
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-13 19:14:56 +0500

    comlited Реклама с картинкой

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def promotion_image():
                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
                         crossorigin="anonymous">
+                    <link rel="stylesheet" href="{url_for('static', filename='css/style.css')}">
                     <title>Привет, Марс!</title>
                   </head>
                   <body>
