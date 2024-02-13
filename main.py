1222c014dafe55aade0400141dba11a6027b05d7
Jlupuka <dima.usachev.0790@mail.ru>
2024-02-13 20:04:25 +0500

    comlited Отбор астронавтов

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import url_for
+from flask import request
 
 app = Flask(__name__)
 
@@ -16,6 +17,17 @@ alerts = ["alert alert-primary",
           "alert alert-info",
           "alert alert-light",
           "alert alert-dark"]
+professions = ('инженер-исследователь, '
+               'пилот, строитель, '
+               'экзобиолог, '
+               'врач, инженер по'
+               ' терраформированию, '
+               'климатолог, специалист '
+               'по радиационной защите, '
+               'астрогеолог, гляциолог, '
+               'инженер жизнеобеспечения, '
+               'метеоролог, оператор марсохода,'
+               ' киберинженер, штурман, пилот дронов').split(', ')
 
 
 @app.route('/')
@@ -75,6 +87,78 @@ def promotion_image():
                   </body>
                 </html>"""
     return title
+
+
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        checkBox = [(f'<div class="form-group form-check">\n  '
+                     f'<input type="checkbox" class="form-check-input" id="acceptRules{index}" '
+                     f'name="profession{index}">\n'
+                     f'<label class="form-check-label" for="acceptRules{index}">{name}</label>\n'
+                     f'</div>')
+                    for index, name in enumerate(professions)]
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Анкета на участие в миссии/title>
+                          </head>
+                          <body>
+                            <h1>Анекта претендента на участие в миссии</h1>
+                            <div>
+                                <form class="login_form" method="post">
+                                    <input type="surname" class="form-control" id="surname" aria-describedby="emailHelp" placeholder="Введите фамилию" name="surname">
+                                    <input type="name" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Введите имя" name="name">
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите email" name="email">
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у Вас образование?</label>
+                                        <select class="form-control" id="classSelect" name="class">
+                                          <option>Начальное</option>
+                                          <option>Общее среднее</option>
+                                          <option>Высшее</option>
+                                        </select>
+                                     </div>
+                                    <label for="form-group">Какие у Вас есть профессии?</label>
+                                    {'\n'.join(checkBox)}
+                                    <label for="form-group">Укажите пол</label>
+                                    <div class="form-group">
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему Вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group form-check">
+                                         <input type="checkbox" class="form-check-input" id="acceptRules" 
+                                         name="accept">
+                                         <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                     </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        print(request.json)
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
